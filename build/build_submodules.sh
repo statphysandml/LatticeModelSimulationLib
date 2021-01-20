341df098879525c73304f0a261b4186c5eb9e3e1
kades <kades@thphys.uni-heidelberg.de>
2021-01-20 12:09:51 +0100

    Update build_submodule

--- build/build_submodules.sh
+++ build/build_submodules.sh
@@ -1,9 +1,5 @@
 git submodule update --init --recursive
 
-# Build ParamHelper
-cd ../external_submodules/MCMCSimulationLib/external_submodules/ParamHelper/build
-bash build.sh
-
 # Build MCMCSimulationLib
 cd ../../../build/
 source build.sh
