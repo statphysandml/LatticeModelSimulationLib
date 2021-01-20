3b8a9cdb228e8867e00a963be7ebbfebc1c420d3
kades <kades@thphys.uni-heidelberg.de>
2021-01-20 12:20:27 +0100

    update build_submodules.sh

--- build/build_submodules.sh
+++ build/build_submodules.sh
@@ -2,7 +2,7 @@ git submodule update --init --recursive
 
 # Build MCMCSimulationLib
 cd ../external_submodules/MCMCSimulationLib/build
-bash build.sh
+source build.sh
 
 # Navigate back to build directory
 cd ../../../build/
